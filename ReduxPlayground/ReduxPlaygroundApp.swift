16e43c0a7151c5490b4e7bb3c55fa5b630e65798
Nikita Yankov <evne.yankovnikita@gmail.com>
2023-05-31 13:53:03 +0300

    Initial Commit

---
+++ ReduxPlayground/ReduxPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxPlaygroundApp.swift
+//  ReduxPlayground
+//
+//  Created by nikita on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReduxPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
